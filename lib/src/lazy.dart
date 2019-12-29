619da35b7be3c24dfe88df539f9f087ba8ed23b8
azilvl <73azim@gmail.com>
2019-12-29 14:28:25 +0330

    added docs

--- lib/src/lazy.dart
+++ lib/src/lazy.dart
@@ -1,14 +1,27 @@
 typedef LazyFactory<T> = T Function();
 
 class Lazy<T> {
+  /// Returns true if the [value] has been created and cached.
   bool get isValueCreated => _isValueCreated;
+
+  /// Returns the evaluated value.
+  /// If the value hasn't been evaluated yet, it runs the [_factory] and gets the value,
+  /// Else it returns the cached value.
   T get value => _isValueCreated ? _value : _createValue();
+
+  /// Returns the evaluated value.
+  /// If the value hasn't been evaluated yet, it runs the [_factory] and gets the value,
+  /// Else it returns the cached value.
   T call() => value;
+
+  /// Takes a function ([_factory]) that returns [T].
+  Lazy(this._factory);
+
   LazyFactory<T> _factory;
   T _value;
   bool _isValueCreated = false;
 
-  Lazy(this._factory);
+  
 
   T _createValue() {
     assert(_factory != null, "Lazy factory shouldn't be null");
@@ -21,7 +34,13 @@ class Lazy<T> {
 
 class MutableLazy<T> extends Lazy<T> {
   MutableLazy(LazyFactory<T> factory) : super(factory);
-  void reEvaluate() => _isValueCreated = false;
+
+  /// Notifies the Lazy object that something that was used in the [_factory] has been changed and it needs to re-evaluate next time it tries to get the object. 
+  //TODO: think of a better name for this.
+  void reEvaluate() {
+    _isValueCreated = false;
+    _value = null;
+  }
 
   @override
   T _createValue() {
