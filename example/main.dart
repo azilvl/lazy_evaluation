8553a5991e987e375127aa8c1b55c50d55c90804
azilvl <73azim@gmail.com>
2020-01-27 03:49:42 +0330

    Format

--- example/main.dart
+++ example/main.dart
@@ -1,25 +1,21 @@
 import 'dart:math' as math;
 import 'package:lazy_evaluation/lazy_evaluation.dart';
 
-
 class Circle {
   Circle(this.radius) {
     _circumference = Lazy(() => math.pi * radius * 2);
     _area = Lazy(() async {
-      // simulating something heavy and asynchronous      
+      // simulating something heavy and asynchronous
       await Future.delayed(const Duration(seconds: 2));
       return math.pi * math.pow(radius, 2);
     });
   }
   final double radius;
 
-  Future<double> get area => _area.value;
-  double get circumference => _circumference.value;
-
   Lazy<Future<double>> _area;
   Lazy<double> _circumference;
-
-  
+  Future<double> get area => _area.value;
+  double get circumference => _circumference.value;
 }
 
 Future<void> main() async {
